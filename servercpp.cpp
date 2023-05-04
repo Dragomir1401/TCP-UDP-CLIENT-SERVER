499bc8e63f2a714c3a3698ffad491102df3d3990
Dragomir Andrei <andrei.dragomir1401@stud.acs.upb.ro>
2023-05-05 00:58:09 +0300

    working server

--- servercpp.cpp
+++ servercpp.cpp
@@ -39,12 +39,24 @@ bool client_is_valid(tcp_client clients[], char client_id[], int &number_of_clie
 
 void disconnect_client(int sockfd)
 {
-    char buffer[6];
-    memset(buffer, 0, 6);
-    strcpy(buffer, "close");
-    int res = send(sockfd, buffer, strlen(buffer) + 1, 0);
-    DIE(res < 0, "res");
-    close(sockfd);
+    string acc = "close";
+
+    // Send size + actual payload
+    char *buffer = (char *)malloc(MAX_SIZE);
+
+    // FIrst 4 bytes of size
+    int size = acc.size() + 1;
+    memcpy(buffer, &size, sizeof(int));
+
+    // Next byes of payload
+    acc.copy(buffer + sizeof(int), size);
+    buffer[size + sizeof(int) - 1] = '\0';
+
+    // Send message
+    int rc = send(sockfd, buffer, size + sizeof(int), 0);
+    DIE(rc < 0, "Unable to send data to TCP client.");
+
+    free(buffer);
 }
 
 bool hasKeyV1(unordered_map<string, queue<message>> map, string key)
@@ -161,6 +173,8 @@ int handle_tcp(int tcp_socket, int epollfd, char buffer[MAX_SIZE], tcp_client cl
         int rc = epoll_ctl(epollfd, EPOLL_CTL_DEL, new_socket, &events[index]);
         DIE(rc < 0, "Unable to remove socket from epoll.");
 
+        close(new_socket);
+
         return -1;
     }
     else
@@ -192,12 +206,12 @@ int handle_tcp(int tcp_socket, int epollfd, char buffer[MAX_SIZE], tcp_client cl
 
 void separate_udp_message(char buffer[MAX_SIZE], message *msg, int size)
 {
-    msg->topic = strtok(buffer, " ");
+    msg->topic = buffer;
 
     int type = (int)buffer[MAX_TOPIC_SIZE];
     int val, sign, exp;
     uint8_t pow, pow1;
-    float fval;
+    double fval;
     char aux[MAX_CONTENT_SIZE];
 
     switch (type)
@@ -231,7 +245,7 @@ void separate_udp_message(char buffer[MAX_SIZE], message *msg, int size)
             pow1--;
         }
         fval = val / (exp * 1.0);
-        sign == 1 ? -fval : fval;
+        fval = sign == 1 ? -fval : fval;
         snprintf(aux, MAX_CONTENT_SIZE, "%1.10g", fval);
         msg->payload = aux;
         break;
@@ -256,17 +270,20 @@ void create_new_message(message &msg, struct sockaddr_in udp_ip_addr)
     msg.port_udp = aux;
 }
 
-bool send_to_clients(tcp_client clients[], int &number_of_clients, message msg,
+void send_to_clients(tcp_client clients[], int &number_of_clients, message msg,
                      unordered_map<string, queue<message>> &inactive_list)
 {
     // Send message to all clients
     for (int i = 0; i < number_of_clients; i++)
     {
         // If client is active and subscribed to topic
-        if (clients[i].active && hasKeyV2(clients[i].topics, msg.topic))
+        if (hasKeyV2(clients[i].topics, msg.topic))
         {
-            send_message(msg, clients[i].socket);
-            return true;
+            fprintf(stderr, "Sending message to client %s.\n", clients[i].id);
+            if (clients[i].active)
+            {
+                send_message(msg, clients[i].socket);
+            }
         }
 
         // If client is not active and subscribed to topic
@@ -279,7 +296,6 @@ bool send_to_clients(tcp_client clients[], int &number_of_clients, message msg,
                 {
                     inactive_list[clients[i].id].push(msg);
                 }
-                return true;
             }
 
             if (clients[i].topics.at(msg.topic) == 1)
@@ -290,8 +306,6 @@ bool send_to_clients(tcp_client clients[], int &number_of_clients, message msg,
             }
         }
     }
-
-    return false;
 }
 
 int handle_udp(int udp_socket, int epollfd, char buffer[MAX_SIZE], tcp_client clients[],
@@ -316,8 +330,7 @@ int handle_udp(int udp_socket, int epollfd, char buffer[MAX_SIZE], tcp_client cl
     separate_udp_message(buffer, &msg, rc);
 
     // Send to clients
-    if (send_to_clients(clients, number_of_clients, msg, inactive_list))
-        return 1;
+    send_to_clients(clients, number_of_clients, msg, inactive_list);
 
     // Success
     return 1;
@@ -343,6 +356,8 @@ int handle_stdin(char buffer[MAX_SIZE], int number_of_clients, tcp_client client
                 // Remove socket from epoll
                 int rc = epoll_ctl(epollfd, EPOLL_CTL_DEL, clients[i].socket, &events[index]);
                 DIE(rc < 0, "Unable to remove socket from epoll.");
+
+                close(clients[i].socket);
             }
         }
 
@@ -360,34 +375,22 @@ int handle_stdin(char buffer[MAX_SIZE], int number_of_clients, tcp_client client
 void handle_commands(char buffer[MAX_SIZE], int number_of_clients, tcp_client clients[], int sockfd)
 {
     char topic_targeted[MAX_TOPIC_SIZE];
-    char sf[MAX_SF_SIZE];
-    char command[MAX_COMMAND_SIZE];
+    int sf;
     memset(topic_targeted, 0, MAX_TOPIC_SIZE);
-    memset(sf, 0, MAX_SF_SIZE);
-    memset(command, 0, MAX_COMMAND_SIZE);
 
-    if (!strncmp(buffer, "subscribe", SUBSCRIBE_COMMAND_LEN))
+    if (buffer[strlen(buffer) - 3] == 'S')
     {
-        // strncpy(topic_targeted, buffer + SUBSCRIBE_COMMAND_LEN + 1, strlen(buffer) - SUBSCRIBE_COMMAND_LEN - 4);
-        // topic_targeted[strlen(buffer) - SUBSCRIBE_COMMAND_LEN - 4] = '\0';
-        // strncpy(sf, buffer + SUBSCRIBE_COMMAND_LEN + 1 + strlen(topic_targeted) + 1,
-        //         1);
-        // sf[MAX_SF_SIZE] = '\0';
-        buffer[strlen(buffer) - 1] = '\0';
-        // Separate words in buffer using strtok
-        char *token = strtok(buffer, " ");
-        memcpy(command, token, strlen(token));
-        command[strlen(command)] = '\0';
+        strncpy(topic_targeted, buffer, strlen(buffer) - 3);
+        if (buffer[strlen(buffer) - 2] == '1')
+        {
+            sf = 1;
+        }
+        else
+        {
+            sf = 0;
+        }
 
-        token = strtok(NULL, " ");
-        memcpy(topic_targeted, token, strlen(token));
-        topic_targeted[strlen(topic_targeted)] = '\0';
-
-        token = strtok(NULL, "\n");
-        memcpy(sf, token, 1);
-
-        fprintf(stderr, "Received subscribe to %s with sf %s.\n", topic_targeted, sf);
-        fprintf(stderr, "%s\n", sf);
+        fprintf(stderr, "Received subscribe from client to %s with sf %d.\n", topic_targeted, sf);
 
         for (int i = 0; i < number_of_clients; i++)
         {
@@ -395,18 +398,18 @@ void handle_commands(char buffer[MAX_SIZE], int number_of_clients, tcp_client cl
             {
                 if (hasKeyV2(clients[i].topics, topic_targeted))
                 {
-                    clients[i].topics.at(topic_targeted) = atoi(sf);
+                    clients[i].topics.at(topic_targeted) = sf;
                 }
                 else
                 {
-                    clients[i].topics.insert(pair<string, int>(topic_targeted, atoi(sf)));
+                    clients[i].topics.insert(pair<string, int>(topic_targeted, sf));
                 }
             }
         }
     }
-    else if (!strncmp(buffer, "unsubscribe", UNSUBSCRIBE_COMMAND_LEN))
+    else if (buffer[strlen(buffer) - 1] == 'U')
     {
-        strncpy(topic_targeted, buffer + UNSUBSCRIBE_COMMAND_LEN + 1, strlen(buffer) - UNSUBSCRIBE_COMMAND_LEN - 2);
+        strncpy(topic_targeted, buffer, strlen(buffer) - 1);
         fprintf(stderr, "Received unsubscribe to topic %s.\n", topic_targeted);
 
         for (int i = 0; i < number_of_clients; i++)
@@ -434,17 +437,18 @@ int receive_from_tcp(char buffer[MAX_SIZE], int sockfd, int number_of_clients, t
         {
             if (clients[i].socket == sockfd)
             {
-                fprintf(stderr, "Client disconnected.\n");
+                printf("Client %s disconnected.\n", clients[i].id);
                 clients[i].active = false;
                 break;
             }
         }
-        // Close connection
-        close(sockfd);
 
         // Remove socket from epoll
         int rc = epoll_ctl(epollfd, EPOLL_CTL_DEL, sockfd, &events[index]);
         DIE(rc < 0, "Unable to remove socket from epoll.");
+
+        // Close connection
+        close(sockfd);
 
         return -1;
     }
